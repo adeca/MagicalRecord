9ab7a8278541f217dddb677d0578a288af8315d9
Agustin de Cabrera <agustindc@gmail.com>
2013-06-18 10:29:20 -0300

    [Samples] Added missing file.

---
+++ Samples/iOS/Application/Controllers/TemperatureCell.h
@@ -0,0 +1,15 @@
+//
+//  TemperatureCell.h
+//  MagicalRecordRecipes
+//
+//  Created by Agustín de Cabrera on 18/06/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TemperatureCell : UITableViewCell
+
+- (void)setTemperatureDataFromDictionary:(NSDictionary*)dictionary;
+
+@end
---
+++ Samples/iOS/Application/Controllers/TemperatureCell.m
@@ -0,0 +1,18 @@
+//
+//  TemperatureCell.m
+//  MagicalRecordRecipes
+//
+//  Created by Agustín de Cabrera on 18/06/2013.
+//
+//
+
+#import "TemperatureCell.h"
+
+@implementation TemperatureCell
+
+- (void)setTemperatureDataFromDictionary:(NSDictionary*)dictionary
+{
+    
+}
+
+@end
