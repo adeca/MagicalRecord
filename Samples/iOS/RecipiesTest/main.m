810f0ed930d882e6c33475e8163222921900b3ba
Agustin de Cabrera <agustindc@gmail.com>
2013-06-18 10:41:44 -0300

    [Samples] Added test target.

---
+++ Samples/iOS/RecipiesTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecipiesTest
+//
+//  Created by Agustín de Cabrera on 18/06/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHUnitIOSAppDelegate class]));
+    }
+}
